73a209468701ac2fa572fa95d48a0fd632e96830
sunny <dance@kakao.com>
2023-07-05 23:24:14 +0900

    init: 스토리북 테스트 추가

--- mobx-ObservableTest/src/App.jsx
+++ mobx-ObservableTest/src/App.jsx
@@ -1,19 +1,38 @@
+import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-// import MetaObject from "./MetaObject.js";
+import { observable } from "mobx";
+import { observer } from "mobx-react";
 import OriginalMetaObject from "./MetaObjectOriginal.js";
+import * as THREE from "three";
 
-function App() {
-  // const originalMeta = new OriginalMetaObject()
-  // const newMeta = new MetaObject()
+const metaObjects = observable([]);
 
 
-  const handleClick = () => {
-    console.log("clicked")
-    const meta = new OriginalMetaObject()
-    console.log(meta)
+const App = observer(() => {
+  const [state, setState] = React.useState(0);
+  const handleClick1 = () => {
+    const newMetaObjects = Array.from({length: 10}, (_, i) =>
+      new OriginalMetaObject());
+    metaObjects.replace(newMetaObjects);
+
+    setState(1);
+    console.log(metaObjects)
   }
+
+  const handleClick2 = () => {
+    console.log(metaObjects[0])
+    metaObjects[0].moveX()
+
+  }
+
+
+  const handleClick3 = () => {
+    console.log(metaObjects[1])
+    metaObjects[1].moveX()
+  }
+
 
   return (
     <>
@@ -27,12 +46,24 @@ function App() {
       </div>
       <h1>Mobx Test</h1>
       <div className="card">
-        <button onClick={handleClick}>
-          클릭시 오브젝트 생성
+        <button onClick={handleClick1}>
+          metaObject 10개 만들기
+        </button>
+        <button onClick={handleClick2}>
+          1번 Metaobject 값 1더하기
+        </button>
+        <button onClick={handleClick3}>
+          2번 Metaobject 값 1더하기
         </button>
       </div>
+      <p>
+        변해야할값: group.position.x { state &&  metaObjects[0].group.position.x}
+      </p>
+      <p>
+        변해야할값: group.position.x { state &&  metaObjects[1].group.position.x}
+      </p>
     </>
   )
-}
+})
 
 export default App
