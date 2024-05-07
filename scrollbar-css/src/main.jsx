da993863eabeaf5d255d417d03d5c02656704a2b
sunny <dance@kakao.com>
2024-05-08 00:40:08 +0900

    🚀simplebar-react 로 모바일에서도 스크롤바 항상 보이도록 설정

--- scrollbar-css/src/main.jsx
+++ scrollbar-css/src/main.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import SimpleBar from "simplebar-react";
+import 'simplebar-react/dist/simplebar.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    <App />
-    <App />
+    <SimpleBar forceVisible="y" autoHide={false} style={{ maxHeight: '100vh' }}>
+      <App />
+      <App />
+      <App />
+    </SimpleBar>
   </React.StrictMode>,
 )
