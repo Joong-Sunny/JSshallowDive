6fff45452c8bfdb478d990ecfab29e86da2b7d92
sunny <dance@kakao.com>
2024-11-30 20:19:43 +0900

    module-federation, 빌드된 remote를 host에서 쓸 수 있도록 설정

--- module-federation/remote/vite.config.js
+++ module-federation/remote/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "remote_app",
+      filename: "remoteEntry.js", // 이 파일이 빌드된 후에 생성되는 파일 이름
+      exposes: {
+        "./Button": "./src/Button",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
